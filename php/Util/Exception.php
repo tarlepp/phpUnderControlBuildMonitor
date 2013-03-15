d872096cb9a589888ee509537022e3c5d2afc5b5
tarlepp <tarmo.leppanen@protacon.fi>
2013-03-15 21:20:06 +0200

    JSON util class and base exception class for all Util classes.

---
+++ php/Util/Exception.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * \php\Util\Exception.php
+ *
+ * @package     phpUnderControlBuildMonitor
+ * @subpackage  Util
+ * @category    Exception
+ */
+namespace phpUnderControlBuildMonitor\Util;
+
+use phpUnderControlBuildMonitor\Core\Exception as CException;
+
+/**
+ * Exception -class
+ *
+ * Generic exception class for phpUnderControlBuildMonitor\Util -classes.
+ *
+ * @package     phpUnderControlBuildMonitor
+ * @subpackage  Util
+ * @category    Session
+ *
+ * @author      Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+class Exception extends CException
+{
+}
---
+++ php/Util/Interfaces/JSON.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * \php\Util\Interfaces\JSON.php
+ *
+ * @package     phpUnderControlBuildMonitor
+ * @subpackage  Util
+ * @category    Interface
+ */
+namespace phpUnderControlBuildMonitor\Util\Interfaces;
+
+/**
+ * Interface for \phpUnderControlBuildMonitor\Util\JSON -class.
+ *
+ * @package     phpUnderControlBuildMonitor
+ * @subpackage  Util
+ * @category    Interface
+ *
+ * @author      Tarmo Leppänen <tarmo.leppanen@protacon.com>
+ */
+interface JSON
+{
+}
